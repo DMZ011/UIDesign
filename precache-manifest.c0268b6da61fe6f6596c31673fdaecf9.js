c718adddf97b9503c884e56d6ec597f0e4a6c218
John Doe <john@doe.com>
2019-02-01 22:02:58 -0500

    Updates

---
+++ precache-manifest.c0268b6da61fe6f6596c31673fdaecf9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/UIDesign/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "35bd8e0bd6f4218a45ff",
+    "url": "/UIDesign/static/js/runtime~main.35bd8e0b.js"
+  },
+  {
+    "revision": "57b3f0cc79328b1742a4",
+    "url": "/UIDesign/static/js/main.57b3f0cc.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/UIDesign/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "57b3f0cc79328b1742a4",
+    "url": "/UIDesign/static/css/main.db7a2184.chunk.css"
+  },
+  {
+    "revision": "17841a50b458bcbab1eb081597b506c5",
+    "url": "/UIDesign/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/tributeapp/precache-manifest.8607ee3fb262e630410da67968042f72.js"
+  "/UIDesign/precache-manifest.c0268b6da61fe6f6596c31673fdaecf9.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/tributeapp/index.html", {
+workbox.routing.registerNavigationRoute("/UIDesign/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
