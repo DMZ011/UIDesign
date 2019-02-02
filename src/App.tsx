152a7bc51845137b21ece71b2277838873035e88
John Doe <john@doe.com>
2019-02-01 22:04:30 -0500

    test

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React haha
+            Learn React haha1
           </a>
         </header>
       </div>
